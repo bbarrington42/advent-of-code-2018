5d72a1d5ee1031d15137d3dec6d98e8db12da441
Bill Barrington <bill.barrington@candidpartners.com>
2018-12-17 14:57:30 -0500

    Day 8, part 2 solved.

--- src/main/scala/top/Day8.scala
+++ src/main/scala/top/Day8.scala
@@ -6,7 +6,9 @@ import scala.io.Source
 
 object Day8 {
 
-  case class Node(children: List[Node], metadata: List[Int])
+  case class Node(name: Int, children: List[Node], metadata: List[Int]) {
+    override def toString: String = s"name: ${name.toChar}, children: $children, metadata: $metadata"
+  }
 
   def parse(lines: List[String]): List[Int] = lines match {
     case Nil => Nil
@@ -17,24 +19,24 @@ object Day8 {
   def parse(file: File): List[Int] =
     parse(Source.fromFile(file).getLines().toList)
 
-  def node(data: List[Int]): (Node, List[Int]) = {
+  def node(name: Int, data: List[Int]): (Node, List[Int]) = {
 
     val ccount = data.head
     val mcount = data.tail.head
 
-    val (c, r1) = children(data.drop(2), Nil, ccount)
+    val (c, r1) = children(name + 1, data.drop(2), Nil, ccount)
     val (m, r2) = metadata(r1, mcount)
-    (Node(c, m), r2)
+    (Node(name, c, m), r2)
   }
 
   def metadata(data: List[Int], count: Int): (List[Int], List[Int]) =
     data.splitAt(count)
 
 
-  def children(data: List[Int], nodes: List[Node], count: Int): (List[Node], List[Int]) = {
-    if (count == 0) (nodes, data) else {
-      val (n, d) = node(data)
-      children(d, n :: nodes, count - 1)
+  def children(name: Int, data: List[Int], nodes: List[Node], count: Int): (List[Node], List[Int]) = {
+    if (count == 0) (nodes.reverse, data) else {
+      val (n, d) = node(name, data)
+      children(name + 1, d, n :: nodes, count - 1)
     }
   }
 
@@ -51,18 +53,15 @@ object Day8 {
 
   /////////// Part 2 ////////////
 
-  def solve2(node: Node): Int =
-    value(node.children, node.metadata)
+  def solve2(node: Node): Int = value(node.children, node.metadata)
 
 
-  def value(children: List[Node], indices: List[Int]): Int = {
-    println(s"children: $children, indices: $indices")
-    children match {
-      case Nil => indices.sum
+  def value(children: List[Node], indices: List[Int]): Int = children match {
+    case Nil => indices.sum
 
-      case _ => indices.foldLeft(0)((z, i) =>
-        if (children.isDefinedAt(i - 1)) z + solve2(children(i - 1)) else z)
-    }
+    case _ => indices.foldLeft(0)((z, i) =>
+      if (children.isDefinedAt(i - 1)) z + solve2(children(i - 1)) else z)
+
   }
 
 
@@ -71,10 +70,10 @@ object Day8 {
   def main(args: Array[String]): Unit = {
     val file = new File("data/day8.txt")
 
-    val data = parse(input)
-      //parse(file)
+    val data = //parse(input)
+    parse(file)
 
-    val root = node(data)
+    val root = node('A', data)
 
     println(solve1(root._1))
 
