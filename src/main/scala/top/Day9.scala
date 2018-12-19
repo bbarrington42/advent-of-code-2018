4a87ac03150c9a3ddb6368e2bbd0c39370caae04
Bill Barrington <bill.barrington@candidpartners.com>
2018-12-19 16:38:04 -0500

    Use Int instead of A.

--- src/main/scala/top/Day9.scala
+++ src/main/scala/top/Day9.scala
@@ -2,14 +2,14 @@ package top
 
 object Day9 {
 
-  case class CircularBuffer[A](pos: Int = 0, items: List[A]) {
+  case class MarbleCircle(pos: Int = 0, marbles: List[Int]) {
 
     // Move current position CCW (-) or CW (+) by the specified amount. Return the updated buffer.
-    def rotate(delta: Int): CircularBuffer[A] = {
-      println(s"rotate: this: $this, delta: $delta")
-      val len = items.length
+    def rotate(delta: Int): MarbleCircle = {
+      //println(s"rotate: this: $this, delta: $delta")
+      val len = marbles.length
       val p = ((pos + delta) % len + len) % len
-      println(s"p: $p")
+      //println(s"p: $p")
       copy(pos = p)
     }
 
@@ -17,29 +17,30 @@ object Day9 {
     // Rotate to the right by one and insert the item between the current element and the item immediately to the right
     // of the current position. The current position points to the new item.
     // If there is no element immediately to the right of the current position, grow the items by one.
-    def add(item: A): CircularBuffer[A] = {
+    def add(item: Int): MarbleCircle = {
       val buf = rotate(1)
-      println(s"buf: $buf")
-      if (buf.items.length - buf.pos == 1) buf.copy(pos = buf.pos + 1, items = (item :: buf.items.reverse).reverse) else {
-        val (front, back) = buf.items.splitAt(buf.pos + 1)
-        println(s"splitAt ${buf.pos + 1} ==> front: $front, back: $back")
-        buf.copy(pos = buf.pos + 1, items = front ++ (item :: back))
+      //println(s"buf: $buf")
+      if (buf.marbles.length - buf.pos == 1)
+        buf.copy(pos = buf.pos + 1, marbles = (item :: buf.marbles.reverse).reverse) else {
+        val (front, back) = buf.marbles.splitAt(buf.pos + 1)
+        //println(s"splitAt ${buf.pos + 1} ==> front: $front, back: $back")
+        buf.copy(pos = buf.pos + 1, marbles = front ++ (item :: back))
       }
     }
 
-    def remove(): CircularBuffer[A] = {
-      val (front, back) = items.splitAt(pos)
-      copy(items = front ++ back.tail)
+    def remove(): MarbleCircle = {
+      val (front, back) = marbles.splitAt(pos)
+      copy(marbles = front ++ back.tail)
     }
 
   }
 
 
-  def play(marble: Int, circle: CircularBuffer[Int]): CircularBuffer[Int] =
+  def play(marble: Int, circle: MarbleCircle): MarbleCircle =
     circle.add(marble)
 
 
-  def loop(count: Int, marble: Int, circle: CircularBuffer[Int]): List[CircularBuffer[Int]] =
+  def loop(count: Int, marble: Int, circle: MarbleCircle): List[MarbleCircle] =
     if (count == 0) Nil else
       circle :: loop(count - 1, marble + 1, play(marble, circle))
 
@@ -47,7 +48,7 @@ object Day9 {
   // 458 players; last marble is worth 71307 points
   def main(args: Array[String]): Unit = {
 
-    val init = CircularBuffer[Int](0, List(0))
+    val init = MarbleCircle(0, List(0))
 
     val r = loop(20, 1, init)
 
