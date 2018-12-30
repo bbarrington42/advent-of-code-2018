47e81494adf213c5fddf3cc4459d1272a9b27d9f
Bill Barrington <bill.barrington@candidpartners.com>
2018-12-30 08:06:44 -0500

    Never give up. :-)

--- src/main/scala/top/Day10.scala
+++ src/main/scala/top/Day10.scala
@@ -3,7 +3,6 @@ package top
 import java.io.File
 
 import scala.io.{Source, StdIn}
-import scala.util.Random
 
 object Day10 {
   // position=< 21373,  53216> velocity=<-2, -5>,
@@ -11,6 +10,8 @@ object Day10 {
     """position=<([^,]+),([^>]+)> velocity=<([^,]+),([^>]+)>""".r
 
   case class Point(x: Int, y: Int, vx: Int, vy: Int)
+
+  case class Intercept(time: Long, coordinate: Int)
 
   def xform(line: String): Point = line match {
     case pointRegex(px, py, vx, vy) => Point(px.trim.toInt, py.trim.toInt, vx.trim.toInt, vy.trim.toInt)
@@ -24,26 +25,32 @@ object Day10 {
   def parse(lines: List[String]): List[Point] =
     lines.map(xform)
 
-  // Calculate the x & y intercepts for a Point.
-  // Values are returned in a tuple of tuples, i.e. ((time, x intercept), (time, y intercept))
-  def intercept(point: Point): ((Long, Int), (Long, Int)) = {
+  // Calculate the x & y intercepts for a Point. (x, y)
+  def intercept(point: Point): (Intercept, Intercept) = {
     // Times
-    val xit = -point.y / point.vy
-    val yit = -point.x / point.vx
-    val xint = point.x + point.vx * xit
-    val yint = point.y + point.vy * yit
-    ((xit, xint), (yit, yint))
+    val xtime = -point.y / point.vy
+    val ytime = -point.x / point.vx
+    val xint = point.x + point.vx * xtime
+    val yint = point.y + point.vy * ytime
+    (Intercept(xtime, xint), Intercept(ytime, yint))
   }
 
-  def intercepts(points: List[Point]): List[((Long, Int), (Long, Int))] =
+  def intercepts(points: List[Point]): List[(Intercept, Intercept)] =
     points.map(intercept)
 
+
   // return ((top, left), (bottom, right))
-  def boundingBox(intercepts: List[((Long, Int), (Long, Int))]): ((Int, Int), (Int, Int)) = {
-    val top = intercepts.minBy { case ((tx, x), (ty, y)) => y }._2._2
-    val left = intercepts.minBy { case ((tx, x), (ty, y)) => x }._1._2
-    val bottom = intercepts.maxBy { case ((tx, x), (ty, y)) => y }._2._2
-    val right = intercepts.maxBy { case ((tx, x), (ty, y)) => x }._1._2
+  def boundingBox(intercepts: List[(Intercept, Intercept)]): ((Intercept, Intercept), (Intercept, Intercept)) = {
+    // Separate the intercepts by lowest time
+    val (xint, yint) = intercepts.foldLeft((List.empty[Intercept], List.empty[Intercept])) { case ((xi, yi), (x, y)) =>
+      if (x.time < y.time) (x :: xi, yi) else (xi, y :: yi)
+    }
+
+    val top = yint.minBy(_.coordinate)
+    val bottom = yint.maxBy(_.coordinate)
+    val left = xint.minBy(_.coordinate)
+    val right = xint.maxBy(_.coordinate)
+
     ((top, left), (bottom, right))
   }
 
@@ -52,7 +59,7 @@ object Day10 {
     // Create an array first
     val array = Array.fill[Char](height, width)('.')
     // Translate the points to the center of the display
-    val OFFSET = (width / 4, height / 4)
+    val OFFSET = (width / 2, height / 2)
     // Update the visible points
     val updated = points.map { case Point(x, y, vx, vy) => Point(x + OFFSET._1, y + OFFSET._2, vx, vy) }.foldLeft(array)((z, p) => {
       if (p.x >= 0 && p.x < width && p.y >= 0 && p.y < height)
@@ -61,22 +68,8 @@ object Day10 {
     updated.map(chars => chars.mkString).mkString("\n")
   }
 
-  def tick(points: List[Point], seconds: Long = 1): List[Point] =
+  def update(points: List[Point], seconds: Long): List[Point] =
     points.map { case Point(px, py, vx, vy) => Point(px + vx * seconds.toInt, py + vy * seconds.toInt, vx, vy) }
-
-  // Pick one point at random. Increment time and sum the distances to all other points.
-  // Continue until the value starts to increase. Use Manhattan distance
-  def magicTime(points: List[Point]): Int = {
-    def loop(time: Int, point: Point, points: List[Point], sum: Int): Int = {
-      val newSum = points.foldLeft(0)((z, p) => z + Math.abs(point.x - p.x) + Math.abs(point.y - p.y))
-      if (newSum > sum) time else
-        loop(time + 1, point, tick(points), newSum)
-    }
-
-    val startPoint = points(Random.nextInt(points.length))
-    val startSum = points.foldLeft(0)((z, p) => z + Math.abs(startPoint.x - p.x) + Math.abs(startPoint.y - p.y))
-    loop(0, startPoint, points, startSum)
-  }
 
 
   // Enter loop to control display with keyboard
@@ -93,8 +86,8 @@ object Day10 {
 
       readChar() match {
         case Some(k) => k match {
-          case 'z' => loop(tick(points, -1))
-          case 'x' => loop(tick(points, 1))
+          case 'z' => loop(update(points, -1))
+          case 'x' => loop(update(points, 1))
           case 'q' | 'Q' =>
 
           case _ => loop(points)
@@ -107,25 +100,21 @@ object Day10 {
     loop(points)
   }
 
-  def timeLoop(initTime: Int, width: Int, height: Int, points: List[Point]): Unit = {
-
-    def getTime(): Int = StdIn.readInt()
+  def timeLoop(width: Int, height: Int, points: List[Point], initTime: Long): Unit = {
 
 
-    def loop(time: Int): Unit = {
+    def loop(time: Long): Unit = {
       println(s"time: $time")
-      println(view(width, height, points.map { case Point(x, y, vx, vy) => Point(x + vx * time, y + vy * time, vx, vy) }))
+      println(view(width, height, update(points, time)))
 
-      loop(getTime())
+      Thread.sleep(1000)
+
+      loop(time + 1)
 
     }
 
     loop(initTime)
   }
-
-  // Calculate the time at which each point comes closest to the origin
-  def timeCalc(p: Point): Long =
-    (-p.x * p.vx - p.y * p.vy) / (p.vx * p.vx + p.vy * p.vy)
 
 
   val data = List(
@@ -165,18 +154,16 @@ object Day10 {
   def main(args: Array[String]): Unit = {
     val file = new File("data/day10.txt")
 
-    val width = 81
-    val height = 25
-
     val points = parse(file)
 
-    println(boundingBox(intercepts(points)))
+    val ((top, left), (bottom, right)) = boundingBox(intercepts(points))
 
-    //    val times = points.map(timeCalc).sorted
-    //
-    //    println(times.mkString("\n"))
+    val width = right.coordinate - left.coordinate
+    val height = bottom.coordinate - top.coordinate
 
-    //timeLoop(10300, width, height, points)
+    val time = List(top, left, bottom, right).minBy(_.time).time
+
+    timeLoop(width / 16, height / 32, update(points, time), time)
 
   }
 }
