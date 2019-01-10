45e4448a0ce7288ea7263adc221a74d3707cf63a
Bill Barrington <bill.barrington@candidpartners.com>
2019-01-10 16:54:39 -0500

    Yay. Day 14, part1 completes in acceptable time.

--- src/main/scala/top/Day14.scala
+++ src/main/scala/top/Day14.scala
@@ -1,47 +1,25 @@
 package top
 
-import java.util.{ArrayDeque, Deque}
-
-import scala.collection.JavaConverters._
-
 object Day14 {
 
-  def lastN(n: Int, deque: Deque[Int]): Array[Int] = {
-    def loop(i: Int, it: Iterator[Int], a: Array[Int]): Array[Int] =
-      if (i == 0) a.reverse else if (it.hasNext) loop(i - 1, it, a :+ it.next()) else loop(i - 1, it, a)
-
-    loop(n, deque.descendingIterator().asScala, Array.emptyIntArray)
-  }
 
   def part1(target: Int): String = {
-    def loop(recipes: Deque[Int], elf1: Int, elf2: Int): String = {
-      if (recipes.size == target + 10)
-        lastN(10, recipes).mkString
+    def loop(recipes: IndexedSeq[Int], elf1: Int, elf2: Int): String = {
+      println(recipes.length)
+      if (recipes.length == target + 10)
+        recipes.takeRight(10).mkString
       else {
-        val x = recipes.toArray()(elf1)
-        val y = recipes.toArray()(elf2)
+        val sum = recipes(elf1) + recipes(elf2)
 
-        val sum = x.asInstanceOf[Int] + y.asInstanceOf[Int]
-        println(s"size: ${recipes.size}")
-        val r = if (sum < 10) {
-          recipes.addLast(sum)
-          recipes
-        } else {
-          recipes.addLast(sum / 10)
-          recipes.addLast(sum % 10)
-          recipes
-        }
-        val a = recipes.toArray()
-        val e1 = (elf1 + a(elf1).asInstanceOf[Int] + 1) % r.size
-        val e2 = (elf2 + a(elf2).asInstanceOf[Int] + 1) % r.size
+        val r = if (sum < 10) recipes :+ sum else recipes :+ sum / 10 :+ sum % 10
+
+        val e1 = (elf1 + recipes(elf1) + 1) % r.size
+        val e2 = (elf2 + recipes(elf2) + 1) % r.size
         loop(r, e1, e2)
       }
     }
 
-    val d = new ArrayDeque[Int]()
-    d.addFirst(7)
-    d.addFirst(3)
-    loop(d, 0, 1)
+    loop(IndexedSeq(3, 7), 0, 1)
   }
 
 
