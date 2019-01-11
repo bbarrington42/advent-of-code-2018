a64b424c1a08b9d7fc98a08b411c146419c5e2d8
Bill Barrington <bill.barrington@candidpartners.com>
2019-01-11 06:21:22 -0500

    Part2, day 14. Doesn't work - checkpoint.

--- src/main/scala/top/Day14.scala
+++ src/main/scala/top/Day14.scala
@@ -5,7 +5,6 @@ object Day14 {
 
   def part1(target: Int): String = {
     def loop(recipes: IndexedSeq[Int], elf1: Int, elf2: Int): String = {
-      println(recipes.length)
       if (recipes.length == target + 10)
         recipes.takeRight(10).mkString
       else {
@@ -22,9 +21,27 @@ object Day14 {
     loop(IndexedSeq(3, 7), 0, 1)
   }
 
+  def part2(target: String): Int = {
+    def loop(n: Int, recipes: IndexedSeq[Int], elf1: Int, elf2: Int): Int = {
+      val x = recipes.takeRight(target.length).foldLeft(0)((z, i) => z * 10 + i).toString
+      println(x)
+      if (x == target) n - 1 else {
+        val sum = recipes(elf1) + recipes(elf2)
+
+        val r = if (sum < 10) recipes :+ sum else recipes :+ sum / 10 :+ sum % 10
+
+        val e1 = (elf1 + recipes(elf1) + 1) % r.length
+        val e2 = (elf2 + recipes(elf2) + 1) % r.length
+        loop(n + 1, r, e1, e2)
+      }
+    }
+
+    loop(1, IndexedSeq(3, 7), 0, 1)
+  }
+
 
   def main(args: Array[String]): Unit = {
-    val r = part1(290431)
+    val r = part2("01245")
 
     println(r)
   }
