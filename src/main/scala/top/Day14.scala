f4805eff690a5ac24306fc8f7f2584b561f50145
Bill Barrington <bill.barrington@candidpartners.com>
2019-01-14 08:55:10 -0500

    .

--- src/main/scala/top/Day14.scala
+++ src/main/scala/top/Day14.scala
@@ -22,21 +22,20 @@ object Day14 {
   }
 
   def part2(target: String): Int = {
-    def loop(n: Int, recipes: IndexedSeq[Int], elf1: Int, elf2: Int): Int = {
-      val x = recipes.takeRight(target.length).mkString
-      println(s"$n: ${recipes.mkString}, $x")
-      if (x == target) n + 1 else {
+    def loop(recipes: IndexedSeq[Int], elf1: Int, elf2: Int): Int = {
+      val x = recipes.takeRight(target.length + 1).mkString
+      if (x.contains(target)) recipes.length - target.length else {
         val sum = recipes(elf1) + recipes(elf2)
 
         val r = if (sum < 10) recipes :+ sum else recipes :+ sum / 10 :+ sum % 10
 
         val e1 = (elf1 + recipes(elf1) + 1) % r.length
         val e2 = (elf2 + recipes(elf2) + 1) % r.length
-        loop(n + 1, r, e1, e2)
+        loop(r, e1, e2)
       }
     }
 
-    loop(0, IndexedSeq(3, 7), 0, 1)
+    loop(IndexedSeq(3, 7), 0, 1)
   }
 
 
